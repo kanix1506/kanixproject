3c71d4f202a38a38d60cce6a7b144caad24d5636
kanix1506 <kanittha_thaotan@cmu.ac.th>
2024-11-30 22:50:15 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int S;
+    int Even=0;
+    int Odd=0;
+
+    while (true){
+        cout << "Enter an integer : ";
+        cin >> S;
+
+        if (S==0){
+            break;
+        }
+        if (S%2==0){
+            Even++;
+        } else {
+            Odd++;
+        }
+    }
+
+    cout << "#Even numbers = "<< Even << "\n";
+    cout << "#Odd numbers = "<< Odd << "\n";
     return 0;
 }
